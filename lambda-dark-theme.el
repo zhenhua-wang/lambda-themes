996acb246bbfb85bf029f3791133b31e6536373f
Colin McLear <mclear@fastmail.com>
2022-04-06 00:32:29 -0400

    Revise themes; remove autothemer
    - Remove autothemer
    - Use function to load theme colors

--- lambda-dark-theme.el
+++ lambda-dark-theme.el
@@ -8,65 +8,11 @@
 
 ;;; Code:
 
-(eval-when-compile
-  (require 'cl-lib))
-
 (require 'lambda-themes)
 
-(lambda-themes-deftheme
- lambda-dark
- "A custom theme."
+(deftheme lambda-dark "Lambda theme, the dark version")
 
- ((((class color) (min-colors #xFFFFFF))        ; col 1 GUI/24bit
-   ((class color) (min-colors #xFF)))           ; col 2 Xterm/256
-
-  (lambda-fg          "#EBE9E7" nil)
-  (lambda-bg          "#141414" nil)
-
-  (lambda-ultralight  "#3A3A41" nil)
-  (lambda-highlight   "#2C2C34" nil)
-  (lambda-lowlight    "#1A1919" nil)
-
-  (lambda-urgent      "#CF6752" nil)
-  (lambda-crucial     "#F2DA61" nil)
-  (lambda-focus       "#4560E6" nil)
-  (lambda-strong      "#F5F2F0" nil)
-  (lambda-meek        "#AFADAF" nil)
-  (lambda-mild        "#2D2D2E" nil)
-  (lambda-faint       "#1F2027" nil)
-
-  (lambda-black       "#000000" nil)
-  (lambda-white       "#FFFFFF" nil)
-  (lambda-red         "#EC6A5E" nil)
-  (lambda-green       "#62C554" nil)
-  (lambda-blue        "#81a1c1" nil)
-  (lambda-yellow      "#F4BF4F" nil)
-  (lambda-orange      "#d08770" nil)
-  (lambda-aqua        "#85CCC6" nil)
-  (lambda-purple      "#9D67E6"  nil)
-
-  ;; (lambda-plain       "#CBCACB" nil)
-
-  )
-
- (custom-theme-set-variables 'lambda-dark
-                             `(ansi-color-names-vector
-                               [,lambda-faint
-                                ,lambda-red
-                                ,lambda-green
-                                ,lambda-yellow
-                                ,lambda-blue
-                                ,lambda-purple
-                                ,lambda-aqua
-                                ,lambda-strong]))
- )
-
-;;;###autoload
-(and load-file-name
-     (boundp 'custom-theme-load-path)
-     (add-to-list 'custom-theme-load-path
-                  (file-name-as-directory
-                   (file-name-directory load-file-name))))
+(lambda-themes-create 'dark 'lambda-dark)
 
 (run-hooks 'lambda-themes-after-load-theme-hook)
 
--- lambda-light-theme.el
+++ lambda-light-theme.el
@@ -7,63 +7,11 @@
 
 
 ;;; Code:
-
-(eval-when-compile
-  (require 'cl-lib))
-
 (require 'lambda-themes)
 
-(lambda-themes-deftheme
- lambda-light
- "A custom theme."
+(deftheme lambda-light "Lambda theme, the light version")
 
- ((((class color) (min-colors #xFFFFFF))        ; col 1 GUI/24bit
-   ((class color) (min-colors #xFF)))           ; col 2 Xterm/256
-
-  (lambda-fg          "#0C0D0D" nil)
-  (lambda-bg          "#FFFCFA" nil)
-
-  (lambda-ultralight  "#E3E1E0" nil)
-  (lambda-highlight   "#EBE9E7" nil)
-  (lambda-lowlight    "#F8F6F4" nil)
-
-  (lambda-urgent      "#B30000" nil)
-  (lambda-crucial     "#5D00DA" nil)
-  (lambda-focus       "#0044CC" nil)
-  (lambda-strong      "#000000" nil)
-  (lambda-meek        "#706F6F" nil)
-  (lambda-mild        "#D1CFCF" nil)
-  (lambda-faint       "#F5F2F0" nil)
-
-  (lambda-black       "#000000" nil)
-  (lambda-white       "#FFFFFF" nil)
-  (lambda-gray        "#56565B" nil)
-  (lambda-red         "#EC6A5E" nil)
-  (lambda-green       "#61C554" nil)
-  (lambda-blue        "#4C4CFF" nil)
-  (lambda-yellow      "#F4BF4F" nil)
-  (lambda-orange      "#ED8811" nil)
-  (lambda-aqua        "#278C87" nil)
-  (lambda-purple      "#833AE6" nil))
-
- (custom-theme-set-variables 'lambda-light
-                             `(ansi-color-names-vector
-                               [,lambda-faint
-                                ,lambda-red
-                                ,lambda-green
-                                ,lambda-yellow
-                                ,lambda-blue
-                                ,lambda-purple
-                                ,lambda-aqua
-                                ,lambda-strong])))
-
-;;;###autoload
-(and load-file-name
-     (boundp 'custom-theme-load-path)
-     (add-to-list 'custom-theme-load-path
-                  (file-name-as-directory
-                   (file-name-directory load-file-name))))
-
+(lambda-themes-create 'light 'lambda-light)
 
 (run-hooks 'lambda-themes-after-load-theme-hook)
 
