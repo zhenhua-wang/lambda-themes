60cf9acc5d27a42d5f8e55d45212633414e3e011
Colin McLear <mclear@fastmail.com>
2022-04-09 22:42:02 -0400

    Rename files & revise colors

---
+++ lambda-dark-theme.el
@@ -0,0 +1,25 @@
+;;; lambda-dark-theme.el --- Lambda-theme dark variant   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Colin McLear
+
+;; Author: Colin McLear <mclear@unl.edu>
+;; Keywords: faces,
+
+
+;;; Code:
+
+(require 'lambda-themes)
+
+(deftheme lambda-dark "Lambda theme, the dark regular version")
+
+(lambda-themes-create 'dark 'lambda-dark)
+
+(run-hooks 'lambda-themes-after-load-theme-hook)
+
+(provide-theme 'lambda-dark)
+
+;; Local Variables:
+;; eval: (when (fboundp 'rainbow-mode) (rainbow-mode +1))
+;; End:
+
+;;; lambda-dark-theme.el ends here
---
+++ lambda-light-theme.el
@@ -0,0 +1,24 @@
+;;; lambda-light-theme.el --- Lambda-theme light variant   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Colin McLear
+
+;; Author: Colin McLear <mclear@unl.edu>
+;; Keywords: faces,
+
+
+;;; Code:
+(require 'lambda-themes)
+
+(deftheme lambda-light "Lambda theme, the light regular version")
+
+(lambda-themes-create 'light 'lambda-light)
+
+(run-hooks 'lambda-themes-after-load-theme-hook)
+
+(provide-theme 'lambda-light)
+
+;; Local Variables:
+;; eval: (when (fboundp 'rainbow-mode) (rainbow-mode +1))
+;; End:
+
+;;; lambda-light-theme.el ends here
